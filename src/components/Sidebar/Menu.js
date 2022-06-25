7cb28166f6f51cb571a05669bc2fc23f039c076c
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-25 15:34:12 +0300

    update icons and menu

--- src/components/Sidebar/Menu.js
+++ src/components/Sidebar/Menu.js
@@ -1,15 +1,32 @@
+import { Icon } from 'Icons';
+
 function Menu() {
   return (
     <nav className="px-2">
         <ul className="flex flex-col">
             <li>
-                <a href="#" className="h-10 flex items-center text-sm font-semibold text-white rounded hover:text-white px-4 bg-active">Ana sayfa</a>
+                <a href="#" className="h-10 flex items-center text-sm font-semibold text-white rounded hover:text-white px-4 bg-active">
+                    <span>
+                        <Icon name="home" />
+                    </span>
+                    Ana sayfa
+                </a>
             </li>
             <li>
-                <a href="#" className="h-10 flex items-center text-sm font-semibold text-link rounded hover:text-white px-4 ">Ara</a>
+                <a href="#" className="h-10 flex items-center text-sm font-semibold text-link rounded hover:text-white px-4 ">
+                    <span>
+                        <Icon name="search" />
+                    </span>
+                    Ara
+                </a>
             </li>
             <li>
-                <a href="#" className="h-10 flex items-center text-sm font-semibold text-link rounded hover:text-white px-4 ">Kitaplığın</a>
+                <a href="#" className="h-10 flex items-center text-sm font-semibold text-link rounded hover:text-white px-4 ">
+                    <span>
+                        <Icon name="library" />
+                    </span>
+                    Kitaplığın
+                </a>
             </li>
         </ul>
     </nav>
