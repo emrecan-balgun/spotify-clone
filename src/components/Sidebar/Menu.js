c2e90f7f2bef9b5b925a7335bdf2049925f7e9cb
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-25 22:04:12 +0300

    refactor menu

--- src/components/Sidebar/Menu.js
+++ src/components/Sidebar/Menu.js
@@ -6,32 +6,39 @@ function Menu() {
         color: "rgb(255 255 255)",
         backgroundColor: "#282828"
     };
+    
 
   return (
     <nav className="px-2">
         <ul className="flex flex-col">
             <li>
-                <NavLink style={({ isActive }) => isActive ? activeStyle : undefined} exact to={"/"} className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4">
-                    <span>
-                        <Icon name="home" />
-                    </span>
-                    Ana sayfa
+                <NavLink exact to={"/"}>
+                    {({ isActive }) => (
+                        <span style={isActive ? activeStyle : undefined} className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4">
+                            { isActive ? <Icon name="homeActive" /> : <Icon name="home" /> }
+                            Ana sayfa
+                        </span>
+                    )}
                 </NavLink>
             </li>
             <li>
-                <NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to={"/search"} className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4 ">
-                    <span>
-                        <Icon name="search" />
-                    </span>
-                    Ara
+                <NavLink exact to={"/search"}>
+                    {({ isActive }) => (
+                        <span style={isActive ? activeStyle : undefined} className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4">
+                            { isActive ? <Icon name="searchActive" /> : <Icon name="search" /> }
+                            Ara
+                        </span>
+                    )}
                 </NavLink>
             </li>
             <li>
-                <NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to={"/collection"} className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4 ">
-                    <span>
-                        <Icon name="collection" />
-                    </span>
-                    Kitaplığın
+                <NavLink exact to={"/collection"}>
+                    {({ isActive }) => (
+                        <span style={isActive ? activeStyle : undefined} className="h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4">
+                            { isActive ? <Icon name="collectionActive" /> : <Icon name="collection" /> }
+                            Kitaplığın
+                        </span>
+                    )}
                 </NavLink>
             </li>
         </ul>
