ba4f21242008da79e8c8173b6adc546e7489952d
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-27 15:27:50 +0300

    update player and add CustomRange components

--- src/components/BottomBar/Player.js
+++ src/components/BottomBar/Player.js
@@ -1,12 +1,13 @@
 import { Icon } from "Icons";
-import { Range, getTrackBackground } from "react-range";
-import { useState } from "react";
+import { useAudio } from "react-use";
+import { secondsToTime } from "utils";
+import CustomRange from '../CustomRange';
 
 function Player() {
-    const [values, setValues] = useState([50]);
-    const STEP = 0.1;
-    const MIN = 0;
-    const MAX = 100;
+    const [audio, state, controls, ref] = useAudio({
+        src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+        autoPlay: true,
+    });
 
   return (
     <div className="flex px-4 justify-between items-center h-full">
@@ -21,8 +22,10 @@ function Player() {
                 <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
                     <Icon size={16} name="playerPrev" />
                 </button>
-                <button className="w-8 h-8 bg-white flex items-center justify-center text-black rounded-full hover:scale-[1.05]">
-                    <Icon size={16} name="play" />
+                <button 
+                    className="w-8 h-8 bg-white flex items-center justify-center text-black rounded-full hover:scale-[1.05]"
+                    onClick={controls[state?.playing ? 'pause' : 'play']}>
+                    <Icon size={16} name={state?.playing ? 'pause' : 'play'} />
                 </button>
                 <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
                     <Icon size={16} name="playerNext" />
@@ -31,47 +34,25 @@ function Player() {
                     <Icon size={16} name="repeat" />
                 </button>
             </div>
-            <div className="w-full">
-            <Range
-          values={values}
-          step={STEP}
-          min={MIN}
-          max={MAX}
-          onChange={(values) => setValues(values)}
-          renderTrack={({ props, children }) => (
-            <div
-              onMouseDown={props.onMouseDown}
-              onTouchStart={props.onTouchStart}
-              className="w-full h-7 flex group"
-              style={props.style}
-            >
-              <div
-                ref={props.ref}
-                className="h-1 w-full rounded-md self-center"
-                style={{
-                  background: getTrackBackground({
-                    values: values,
-                    colors: ["#1db954", "#535353"],
-                    min: MIN,
-                    max: MAX
-                  })
-                }}
-              >
-                {children}
-              </div>
-            </div>
-          )}
-          renderThumb={({ props, isDragged }) => (
-            <div
-              {...props}
-              className={`h-3 w-3 rounded-full bg-white ${!isDragged ? 'opacity-0' : ''} group-hover:opacity-100`}
-              style={{
-                ...props.style,
-                boxShadow: "0 2px 4px 0 rgb(0 0 0 / 50%)"
-              }}
-            />
-          )}
-        />
+            <div className="w-full flex items-center gap-x-2">
+                {audio}
+                <div className="text-[0.688rem] text-white text-opacity-70">
+                    {
+                        secondsToTime(state?.time)
+                    }
+                </div>
+                <CustomRange 
+                    step={0.1}
+                    min={0}
+                    max={state?.duration || 1}
+                    value={state?.time}
+                    onChange={value => controls.seek(value)}
+                />
+                <div className="text-[0.688rem] text-white text-opacity-70">
+                    {
+                        secondsToTime(state?.duration)
+                    }
+                </div>
             </div>
         </div>
         <div className="min-w-[11.25rem] w-[30%] flex justify-end">
