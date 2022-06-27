84538dbd3127817e4e2dc0e2059fe141f7c40e54
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-27 14:58:48 +0300

    refactor player.js

--- src/components/BottomBar/Player.js
+++ src/components/BottomBar/Player.js
@@ -1,12 +1,19 @@
-import { Icon } from 'Icons';
+import { Icon } from "Icons";
+import { Range, getTrackBackground } from "react-range";
+import { useState } from "react";
 
 function Player() {
+    const [values, setValues] = useState([50]);
+    const STEP = 0.1;
+    const MIN = 0;
+    const MAX = 100;
+
   return (
     <div className="flex px-4 justify-between items-center h-full">
         <div className="min-w-[11.25rem] w-[30%]">
             sol
         </div>
-        <div className="flex flex-col items-center">
+        <div className="max-w-[45.125rem] w-[40%] flex flex-col items-center">
             <div className="flex items-center gap-x-2">
                 <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
                     <Icon size={16} name="shuffle" />
@@ -24,8 +31,47 @@ function Player() {
                     <Icon size={16} name="repeat" />
                 </button>
             </div>
-            <div>
-                alt
+            <div className="w-full">
+            <Range
+          values={values}
+          step={STEP}
+          min={MIN}
+          max={MAX}
+          onChange={(values) => setValues(values)}
+          renderTrack={({ props, children }) => (
+            <div
+              onMouseDown={props.onMouseDown}
+              onTouchStart={props.onTouchStart}
+              className="w-full h-7 flex group"
+              style={props.style}
+            >
+              <div
+                ref={props.ref}
+                className="h-1 w-full rounded-md self-center"
+                style={{
+                  background: getTrackBackground({
+                    values: values,
+                    colors: ["#1db954", "#535353"],
+                    min: MIN,
+                    max: MAX
+                  })
+                }}
+              >
+                {children}
+              </div>
+            </div>
+          )}
+          renderThumb={({ props, isDragged }) => (
+            <div
+              {...props}
+              className={`h-3 w-3 rounded-full bg-white ${!isDragged ? 'opacity-0' : ''} group-hover:opacity-100`}
+              style={{
+                ...props.style,
+                boxShadow: "0 2px 4px 0 rgb(0 0 0 / 50%)"
+              }}
+            />
+          )}
+        />
             </div>
         </div>
         <div className="min-w-[11.25rem] w-[30%] flex justify-end">
