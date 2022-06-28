267d8675c8183473cbc43a5441f44f8d4f124dcc
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-28 14:54:42 +0300

    add search button, categories and update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
 import Navigation from "./Navbar/Navigation"
 import Auth from "./Navbar/Auth"
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import Search from "./Navbar/Search";
+
 
 function Navbar() {
+  let location = useLocation();
+
   return (
     <nav className="h-[3.7rem] flex items-center justify-between px-8">
         <Navigation />
-        <Routes>
+        {
+          location.pathname === "/search" && (
+            <Search />
+          )
+        }
+        {/* <Routes>
           <Route exact path="/" >Home</Route>
           <Route path="/search" >Search</Route>
           <Route path="/collection" >Collection</Route>
-        </Routes>
+        </Routes> */}
         <Auth />
     </nav>
   )
---
+++ src/components/Navbar/Search.js
@@ -0,0 +1,14 @@
+import { Icon } from 'Icons';
+
+function Search() {
+  return (
+    <div className="mr-auto ml-4 relative">
+        <label htmlFor="search-input" className="text-black w-12 h-10 flex items-center justify-center absolute top-0 left-0">
+            <Icon name="search" size={24} />
+        </label>
+        <input type="text" id="search-input" className="h-10 pl-12 bg-white rounded-3xl outline-none text-black placeholder-black/50 text-sm max-w-full w-[22.75rem]" placeholder="Sanatçılar, şarkılar veya podcast'ler" />
+    </div>
+  )
+}
+
+export default Search
